4d66f564a3d58d2400a3ed2c47a740337de2d459
a.vitsiuk <a.vitsiuk@affidaty.io>
2022-08-01 16:47:05 +0200

    added 2 new methods to client, fixed error getting genesis block info

--- src/systemDefaults.ts
+++ src/systemDefaults.ts
@@ -1,2 +1,6 @@
 export const ROOT_CA_ID = '';
 export const SERVICE_ACCOUNT_ID = 'TRINCI';
+export const NODE_API_PATH_COMMON = '/api/v1';
+export const SUBMIT_MESSAGE_PATH = `${NODE_API_PATH_COMMON}/message`;
+export const NODE_VISA_PATH = `${NODE_API_PATH_COMMON}/visa`;
+export const BOOTSTRAP_DL_PATH = `${NODE_API_PATH_COMMON}/bootstrap`;
