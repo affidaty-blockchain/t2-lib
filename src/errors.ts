67cf4f8d5425744003cbb4c41ca94c8f9634f3f2
a.vitsiuk <a.vitsiuk@affidaty.io>
2022-08-16 13:04:18 +0200

    added timeout error

--- src/errors.ts
+++ src/errors.ts
@@ -44,3 +44,4 @@ export const UNKNOWN_TX = 'Unknown transaction.';
 export const NOT_SUB_TO_BLOCKS = 'Not subscribed to blocks.';
 export const NO_BRIDGE_IN_BROWSER = 'Bridge is not available in browser context.';
 export const UNSURE_HASH_TYPE = 'Unsure about what hash type to use during sign/verify.';
+export const TIMEOUT_ERR = 'Timeout reached.';
