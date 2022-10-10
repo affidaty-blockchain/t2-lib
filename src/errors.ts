457473b10fed74b7ea2e1aa0df7ad0c6df73d9ef
a.vitsiuk <a.vitsiuk@affidaty.io>
2022-10-11 00:12:11 +0200

    added error message

--- src/errors.ts
+++ src/errors.ts
@@ -45,3 +45,4 @@ export const NOT_SUB_TO_BLOCKS = 'Not subscribed to blocks.';
 export const NO_BRIDGE_IN_BROWSER = 'Bridge is not available in browser context.';
 export const UNSURE_HASH_TYPE = 'Unsure about what hash type to use during sign/verify.';
 export const REQ_ABORT_ERR = 'Request aborted.';
+export const UNKNOWN_TX_TYPE = 'Unknown transaction type.';
